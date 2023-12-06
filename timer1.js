01fe8c4a72de3a8947cdef79ab706158da10ee43
websoket <114286773+websoket@users.noreply.github.com>
2023-12-07 09:52:52 +1100

    Update timer1.js

--- timer1.js
+++ timer1.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("2023-12-17 10:10:25").getTime();
+var countDownDate = new Date("12/17/2023 10:10:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "not any time";
   }
 }, 1000);
