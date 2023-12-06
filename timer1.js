07007bbfd8a189637f869c56b99c5196fe2bc41f
websoket <114286773+websoket@users.noreply.github.com>
2023-12-07 09:13:05 +1100

    right time

--- timer1.js
+++ timer1.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+var countDownDate = new Date("December 17, 2023 10:00:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
